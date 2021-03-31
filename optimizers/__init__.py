0faa576c6465bc39cb2c2d14e4a6e36f37da57ce
matthewwicker <matthewwicker@cs.ox.ac.uk>
2021-03-31 14:13:49 +0100

    aliased inference methods

--- optimizers/__init__.py
+++ optimizers/__init__.py
@@ -1,8 +1,20 @@
 #from optimizer import Optimizer
 from .bayesbybackprop import BayesByBackprop
+from .bayesbybackprop import BayesByBackprop as BBB
+
 from .noisyadam import NoisyAdam
+from .noisyadam import NoisyAdam as NA
+
 from .blrvi import VariationalOnlineGuassNewton
+from .blrvi import VariationalOnlineGuassNewton as VOGN
+
 from .sgd import StochasticGradientDescent
+from .sgd import StochasticGradientDescent as SGD
+
 from .swag import StochasticWeightAveragingGaussian
+from .swag import StochasticWeightAveragingGaussian as SWAG
+
 from .hmc import HamiltonianMonteCarlo
+from .hmc import HamiltonianMonteCarlo as HMC
+
 from .adam import Adam
