90040649bf40dc00d72b27673866b6d62259618b
matthewwicker <matthewwicker@cs.ox.ac.uk>
2021-04-03 04:35:46 +0100

    updated with the expectation bound :)

--- analyzers/bounds.py
+++ analyzers/bounds.py
@@ -89,7 +89,78 @@ def chernoff_decision_robustness(model, input_0, input_1=None, verify=True, **kw
     Function to compute the decision robustness of a given Bayesian posterior
     with statistically guarenteed precision via Chernoff concentration
     """
-    return None
+    if(model.det):
+        raise ValueError("Error computing bounds: model robustness is strictly for stochastic networks!")
+    # Property Parameter
+    epsilon_ball = kwargs.get('eps', 0.0)
+    # Bound Parameters
+    epsilon = kwargs.get('epsilon', 0.1)
+    delta = kwargs.get('delta', 0.05)
+
+    # Optional Parameters
+    regression = kwargs.get('regression', False)	# if the BNN is on a regression task
+    output_l = kwargs.get('output_l', None)
+    output_u = kwargs.get('output_u', None)
+    output_cls = kwargs.get('output_cls', None)
+
+    approx = kwargs.get('approx', False)		# if the user wants to perform approx IBP
+    loss_fn = kwargs.get('loss', None)
+
+    # if no upper bound is supplied then we assume the user wants epsilon ball
+    if(input_1 is None):
+        input_1 = input_0 + epsilon_ball
+        input_0 = input_0 - epsilon_ball
+
+    if(regression == True and (output_l is None or output_u is None)):
+        raise ValueError("Error computing bounds: regression was selected but no safe set was specified.")
+    elif(regression == False and output_cls is None):
+        print("[deepbayes] Warning: no class supplied when checking property. Stability condition assumed.")
+        output_cls = np.argmax(model.predict((input_0+input_1)/2.0))
+    if(verify == False and loss_fn is None):
+        raise ValueError("Error computing bounds: using attack to approx worst case but no loss function was specified.")
+    if(regression == False):
+        num_classes = model.model.get_weights()[-1].shape[-1]
+
+    chernoff_bound = math.ceil( (1/(2*epsilon**2)) * math.log(2/delta) )
+    estimate = 0.0
+    worst_out = 0.0; worst_l = 0.0; worst_u = 0.0
+    for i in trange(chernoff_bound, desc="Sampling for Chernoff Bound Satisfaction"):
+        model.set_weights(model.sample())
+        # Perform the testing here
+        if(verify == True):
+            out_l, out_u = IBP(model, input_0, input_1, model.model.get_weights(), predict=regression, approx=approx)
+            if(not regression):
+                v1 = tf.one_hot(output_cls, depth=num_classes)
+                v2 = 1 - v1
+                worst_case = tf.math.add(tf.math.multiply(v2, out_u), tf.math.multiply(v1, out_l))
+                softmax = model.model.layers[-1].activation(tf.reshape(worst_case, (1,num_classes)))
+                worst_out += softmax
+            else:
+                worst_l += out_l; worst_u += out_u
+        else:
+            adv_example = FGSM(model, (input_0+input_1)/2.0, loss_fn=loss_fn, eps=epsilon_ball)
+            worst_case = model.predict(adv_example)
+            worst_out += worst_case
+    if(not regression and (np.argmax(worst_case) == output_cls)):
+        return 1.0
+    elif(regression and ((worst_case >= output_l).all() and (worst_case <= output_u).all())):
+        return 1.0
+    return 0.0
+
+
+# This is h_a in the paper
+def _absolute_massart_halting(succ, trials, I, epsilon, delta, alpha):
+    gamma = float(succ)/trials
+    if(I[0] < 0.5 and I[1] > 0.5):
+        return -1
+    elif(I[1] < 0.5):
+        val = I[1]
+        h = (9/2.0)*(((3*val + epsilon)*(3*(1-val)-epsilon))**(-1))
+        return math.ceil((h*(epsilon**2))**(-1) * math.log((delta - alpha)**(-1)))
+    elif(I[0] >= 0.5):
+        val = I[0]
+        h = (9/2.0)*(((3*(1-val) + epsilon)*((3*val)+epsilon))**(-1))
+        return math.ceil((h*(epsilon**2))**(-1) * math.log((delta - alpha)**(-1)))
 
 def massart_model_robustness(model, input_0, input_1=None, verify=True, **kwargs):
     """
